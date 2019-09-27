727a4c5c4a89958da2e8422bb158711dc9ce1d5a
UnknownMonk <smodcartoon@gmail.com>
2019-09-27 09:25:48 -0400

    kata added

---
+++ countingSheep.js
@@ -0,0 +1,39 @@
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+
+  arrayOfSheep.forEach(sheep => {
+    if (sheep === true) {
+      count++;
+    }
+  });
+  return count;
+}
+
+var array1 = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true
+];
+
+console.log(countSheeps(array1) == 17, "There are 17 sheeps in total");
