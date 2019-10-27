9f83a3a87fdae1f9db35480cc86ce9a9e6289901
UnknownMonk <smodcartoon@gmail.com>
2019-10-27 09:38:26 -0400

    kata 100

--- 5-25-2019/divisibaleBygivenNumber.js
+++ 5-25-2019/divisibaleBygivenNumber.js
@@ -8,8 +8,8 @@ function divisibleBy(numbers, divisor) {
   return dividedBy;
 }
 
-console.col(divisibleBy([1, 2, 3, 4, 5, 6], 2), [2, 4, 6]);
-console.col(divisibleBy([1, 2, 3, 4, 5, 6], 3), [3, 6]);
-console.col(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4), [0, 4]);
-console.col(divisibleBy([0], 4), [0]);
-console.col(divisibleBy([1, 3, 5], 2), []);
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2), [2, 4, 6]);
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 3), [3, 6]);
+console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4), [0, 4]);
+console.log(divisibleBy([0], 4), [0]);
+console.log(divisibleBy([1, 3, 5], 2), []);
